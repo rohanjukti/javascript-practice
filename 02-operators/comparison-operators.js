b1784c0a91ab34dd301b19aaea8f94500a3b2aa0
rohanjukti <rjukti11@gmail.com>
2026-02-26 20:56:00 +0530

    Comparison operators in js

---
+++ 02-operators/comparison-operators.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 5;
+
+console.log("Equal (==):", a == b);                   //false
+console.log("Strict Equal (===):", a === b);          //false
+
+console.log("Not Equal (!=):", a != b);               //true
+console.log("Strict Not Equal (!==):", a !== b);      //true
+
+console.log("Greater Than (>):", a > b);              //true
+console.log("Less Than (<):", a < b);                 //false
+
+console.log("Greater Than or Equal (>=):", a >= b);   //true
+console.log("Less Than or Equal (<=):", a <= b);      //false
