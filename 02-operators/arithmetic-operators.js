623945f1b6b9cea96259f81b49ccb39aaecbd3d6
rohanjukti <rjukti11@gmail.com>
2026-02-26 20:43:57 +0530

    arithmetic oprators in js

--- 02-operators/arithmetic-operators.js
+++ 02-operators/arithmetic-operators.js
@@ -1,8 +1,8 @@
 let a = 10;
 let b = 5;
 
-console.log("Addition:", a + b);
-console.log("Subtraction:", a - b);
-console.log("Multiplication:", a * b);
-console.log("Division:", a / b);
-console.log("Remainder:", a % b);
+console.log("Addition:", a + b);          //15
+console.log("Subtraction:", a - b);       //5
+console.log("Multiplication:", a * b);    //50
+console.log("Division:", a / b);          //2
+console.log("Remainder:", a % b);         //0
