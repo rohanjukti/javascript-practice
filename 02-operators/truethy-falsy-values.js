36d128b15259136d9e4da6a364e314abeef3395c
Rohan Jukti <rjukti11@gmail.com>
2026-02-27 12:19:32 +0530

    Add truethy and falsy values examples in JS

---
+++ 02-operators/truethy-falsy-values.js
@@ -0,0 +1,18 @@
+truthy values
+
+if ("0")        //  Truthy
+if (" ")        //  Truthy (space bhi truthy hai)
+if ([])         //  Truthy
+if ({})         //  Truthy
+
+
+falsy values
+
+false   //falsy
+0       //falsy
+-0      //falsy
+0n     //falsy
+""      //falsy
+null    //falsy
+undefined   //falsy
+NaN       //falsy
